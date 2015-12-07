7905f97c7d12e31b1cf80a08275ff23a7ba289f3
ga2arch <gabriele.carrettoni@gmail.com>
2015-12-07 16:18:57 +0100

    first commit

---
+++ src/Main.hs
@@ -0,0 +1,82 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Main where
+
+import           Blaze.ByteString.Builder
+import           Blaze.ByteString.Builder.ByteString
+import           Blaze.ByteString.Builder.Int
+import           Control.Concurrent
+import           Control.Concurrent.Async
+import           Control.Concurrent.MVar
+import           Control.Monad
+import           Control.Monad.IO.Class
+import           Data.ByteString                     (ByteString)
+import qualified Data.ByteString.Char8               as C
+import qualified Data.HashMap.Strict                 as H
+import           Data.Maybe
+import           Data.Monoid
+import           Network.HTTP.Types.Status
+import           Network.Socket                      hiding (recv, recvFrom,
+                                                      send, sendTo)
+import           Network.Socket.ByteString
+import qualified Web.Scotty                          as S
+
+type User       = String
+type UserSocket = MVar (H.HashMap User Socket)
+type HostUser   = MVar (H.HashMap HostName User)
+
+main :: IO ()
+main = do
+  mus <- newMVar H.empty
+  mhu <- newMVar H.empty
+  async $ httpServer mus mhu
+  socketServer mus mhu
+
+socketServer mus mhu = do
+  sock <- socket AF_INET Stream 0
+  setSocketOption sock ReuseAddr 1
+  setSocketOption sock KeepAlive 1
+  bindSocket sock (SockAddrInet 8888 iNADDR_ANY)
+  listen sock 2
+  forever $ do
+    conn <- accept sock
+    forkIO $ runConn conn mus mhu
+
+runConn :: (Socket, SockAddr) -> UserSocket -> HostUser -> IO ()
+runConn (sock, (SockAddrInet _ host)) mus mhu = do
+  hostUser <- readMVar mhu
+  address  <- inet_ntoa host
+  putStrLn $ "Received new connetion from " ++ (show address)
+
+  case H.lookup address hostUser of
+    Just user -> modifyMVar_ mus $ \userSocket -> do
+         when (H.member user userSocket) (close (fromJust $ H.lookup user userSocket))
+         return $ H.insert user sock userSocket
+    Nothing -> close sock
+
+sendPush :: Socket -> ByteString -> IO ()
+sendPush socket message = do
+  putStrLn $ "Pushing: " ++ (show message)
+  connected <- isConnected socket
+  when connected (do
+    let size = fromIntegral $ C.length message
+    let push = writeToByteString (writeInt32be size <> writeByteString message)
+    void $ send socket push)
+
+httpServer mus mhu = S.scotty 9000 $ do
+  S.post "/enable" (do
+    userid <- S.param "user_id" :: S.ActionM String
+    host   <- S.param "from"    :: S.ActionM HostName
+
+    liftIO $ putStrLn $ "Enabling: " ++ (show userid)
+    liftIO $ modifyMVar_ mhu $ \hostUser ->
+      return $ H.insert host userid hostUser
+    S.status status200)
+
+  S.post "/push" (do
+    userid  <- S.param "user_id"
+    message <- S.param "message"
+
+    liftIO $ modifyMVar_ mus $ \userSocket -> do
+      let socket = H.lookup userid userSocket
+      when (isJust socket) (sendPush (fromJust socket) message)
+      return userSocket)
